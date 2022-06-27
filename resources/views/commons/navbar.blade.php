bdd37fdd08d48cfae528c44a8c8efc218a360c17
yukina.sakurai <ys1201.kannadki@gmail.com>
2022-06-27 08:09:37 +0000

    navbar1

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,24 @@
+<div class="b-example-divider"></div>
+
+  <div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center col-md-4 mb-2 mb-md-0 text-dark text-decoration-none">
+        <h3>SAZANAMI coffee shop</h3>
+      </a>
+
+
+      <dir class="offset-md-4 col-md-4">
+            <button type="button" class="btn btn-outline-primary me-2">Login</button>
+            <button type="button" class="btn btn-primary">Logout</button>
+            <button type="button" class="btn btn-outline-primary me-2">会員登録</button>
+      
+            <button type="button" class="btn ">
+                  <i class="fab fa-twitter-square"></i>
+            </button>
+            <button type="button" class="btn text-end">
+                  <i class="fab fa-instagram-square"></i>
+            </button>
+      </dir>
+      
+    </header>
+ </div>
